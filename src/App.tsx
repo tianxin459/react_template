d8ee405966213b425b3723d5edf71828196fad96
Ellis <onlyheart@qq.com>
2022-09-02 13:07:57 +0800

    add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import React, { useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, useAppDispatch } from "./redux/store";
+import { getData } from "./redux/slice/testSlice";
+ 
 function App() {
+  let data = useSelector((s: RootState) => s.test.data);
+  let dispatch = useAppDispatch();
+  useEffect(() => {
+    dispatch(getData());
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {data.map((x) => (
+      <div key={x.name}>{x.name}</div>
+    ))}
     </div>
   );
 }
